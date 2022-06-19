750ecf8a041abe4e3eb3598f61960950f5c330b5
Michael Chen <michael.chen.1120@gmail.com>
2022-06-19 01:15:24 -0400

    add users and sessions and frontend folder

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if @user.nil?
+      render json: ['Nope. Wrong credentials!'], status: 401
+    else
+      login!(@user)
+      render 'api/users/show';
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout!
+      render json: {}
+    else
+      render json: "No users are currently logged in", status: 404
+    end
+  end
+end
