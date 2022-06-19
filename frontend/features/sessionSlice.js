959241c22650efb343a40b82a3ef4e1b0fd2a43a
Michael Chen <michael.chen.1120@gmail.com>
2022-06-19 02:27:16 -0400

    learning redux toolkit file structure

---
+++ frontend/features/sessionSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const sessionSlice = createSlice({
+  name: 'session',
+  initialState: {
+    id: null,
+  },
+  reducers: {},
+});
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,3 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './app/store';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  const configureStore = store();
+  ReactDOM.render(<Root store={configureStore} />, root);
+});
