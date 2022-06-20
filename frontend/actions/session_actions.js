86fd9bb647a794451257ebea3c056e14928294a9
Michael Chen <michael.chen.1120@gmail.com>
2022-06-19 22:07:14 -0400

    add thunk action creators for session

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,6 +1,21 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 import * as APIUtil from '../util/session_api_util';
 
 export const receiveCurrentUser = createAction('receiveCurrentUser');
 export const logoutCurrentUser = createAction('logoutCurrentUser');
 export const receiveErrors = createAction('receiveSessionErrors');
+
+export const signup = createAsyncThunk('user/signup', async (user) => {
+  const response = await APIUtil.signup(user);
+  return response;
+});
+
+export const login = createAsyncThunk('user/login', async (user) => {
+  const response = await APIUtil.login(user);
+  return response;
+});
+
+export const logout = createAsyncThunk('user/logout', async () => {
+  const response = await APIUtil.logout();
+  return response;
+});
