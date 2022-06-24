fb3e240067da130881f71da57948e1b6fd095973
Michael Chen <michael.chen.1120@gmail.com>
2022-06-24 10:27:06 -0400

    Finish splash page mostly

--- frontend/features/components/splash/splash.jsx
+++ frontend/features/components/splash/splash.jsx
@@ -24,7 +24,7 @@ const Splash = (props) => {
             <br />
             <br />
             <br />
-            <span className="bold-text">Slack is free to try </span> for as long
+            <span className="bold-text">SlaQ is free to try </span> for as long
             as you’d like
           </h3>
           <br />
@@ -36,9 +36,82 @@ const Splash = (props) => {
           />
         </video>
       </div>
-      <div className="splash-section-top"></div>
-      <div className="splash-section-mid"></div>
-      <div className="splash-section-bottom"></div>
+      <div className="splash-section-top">
+        <div className="splash-section-container">
+          <video autoPlay muted loop playsInline>
+            <source
+              src="https://slaq-objects.s3.amazonaws.com/splash-section-1.webm"
+              type="video/webm"
+            />
+          </video>
+          <div className="splash-section-1-text">
+            <h2 className="splash-section-header">
+              Bring your team <br /> together{' '}
+            </h2>
+            <p className="splash-section-text">
+              At the heart of SlaQ are channels: organized spaces for <br />
+              everyone and everything you need for work. In channels, it’s{' '}
+              <br />
+              easier to connect across departments, offices, time zones <br />
+              and even other companies.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="splash-section-mid">
+        <div className="splash-section-container">
+          <div className="splash-section-2-text">
+            <h2 className="splash-section-header">
+              Choose how you <br /> want to work
+            </h2>
+            <p className="splash-section-text">
+              In Slack, you’ve got all the flexibility to work when, where{' '}
+              <br /> and how it’s best for you. You can easily chat, send audio
+              and <br /> video clips, or hop on a huddle to talk things out
+              live.
+            </p>
+          </div>
+          <video autoPlay muted loop playsInline>
+            <source
+              src="https://slaq-objects.s3.amazonaws.com/splash-section-2.webm"
+              type="video/webm"
+            />
+          </video>
+        </div>
+      </div>
+      <div className="splash-section-bottom">
+        <div className="splash-section-container">
+          <video autoPlay muted loop playsInline>
+            <source
+              src="https://slaq-objects.s3.amazonaws.com/splash-section-3.webm"
+              type="video/webm"
+            />
+          </video>
+          <div className="splash-section-3-text">
+            <h2 className="splash-section-header">
+              Move faster with your <br /> tools in one place
+            </h2>
+            <p className="splash-section-text">
+              With your other work apps connected to SlaQ, you can <br /> work
+              faster by switching tabs less. And with powerful tools <br /> like
+              Workflow Builder, you can automate away routine tasks.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="splash-footer">
+        <div className="splash-footer-container">
+          <div className="splash-footer-text">
+            Welcome to your new digital HQ
+          </div>
+          <div className='splash-footer-ellipse'>
+          </div>
+        </div>
+      </div>
+      <div className="disclaimer-text-container">
+        <p>This clone is for educational purposes only.</p>
+        <p>Please do not put any sensitive information.</p>
+      </div>
     </div>
   );
 };
