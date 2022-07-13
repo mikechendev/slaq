c1a2b7c6e3347399cdd38e97531bff121fb41510
Michael Chen <michael.chen.1120@gmail.com>
2022-07-13 11:23:51 -0400

    Try to recreate error

--- frontend/util/chat_api_util.js
+++ frontend/util/chat_api_util.js
@@ -5,3 +5,5 @@ export const fetchChat = (chatId) => axios.get(`/api/chats/${chatId}`);
 export const createChat = (chat) => axios.post('/api/chats', { chat });
 export const updateChat = (chat) => axios.patch(`/api/chats/${chat.id}`, { chat });
 export const removeChat = (chatId) => axios.delete(`/api/chats/${chatId}`);
+
+
