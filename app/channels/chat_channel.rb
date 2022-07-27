e8c266c16f4d08c98a43020f515fb82a569b22c3
Michael Chen <michael.chen.1120@gmail.com>
2022-07-27 17:34:44 -0400

    Fix chat channels broadcast

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,21 +1,25 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    stream_for 'chat_channel'
-  end
-
-  def speak(data)
-    message = Message.new(body: data['message'])
-    if message.save
-      socket = { message: message.body, type: 'message' }
-      ChatChannel.broadcast_to('chat_channel', socket)
+    current_user.chats.each do |chat|
+      stream_for chat
     end
   end
 
-  def load
-    messages = Message.all.collect(&:body)
-    socket = {messages: messages, type: 'messages'}
-    ChatChannel.broadcast_to('chat_channel', socket)
-  end
+  # def speak(data)
+  #   message = Message.new(body: data['message'])
+  #   if message.save
+  #     socket = { message: message.body, type: 'message' }
+  #     ChatChannel.broadcast_to('chat_channel', socket)
+  #   end
+  # end
 
-  def unsubscribed; end
+  # def load
+  #   messages = Message.all.collect(&:body)
+  #   socket = {messages: messages, type: 'messages'}
+  #   ChatChannel.broadcast_to('chat_channel', socket)
+  # end
+
+  def unsubscribed
+    stop_all_streams
+  end
 end
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,21 +1,26 @@
 class Api::MessagesController < ApplicationController
+
   skip_before_action :verify_authenticity_token
+
   def create
     @channel = Chat.find(@message[:chat_id])
-    @message = @channel.messages.create(message_params)
-    @message.user = current_user
-  
-    ChatChannel.broadcast_to(@channel, @message)
-    render json: @message
-  end
+    @message = @channel.messages.new(message_params)
+    @message.user_id = current_user.id
 
-  def index
-  end
-
-    private
-
-    def message_params
-      params.require(:message).permit(:body, :user_id, :chat_id)
+    if @message.save
+      ChatChannel.broadcast_to(@channel, @message)
+      render json: @message
     end
+  end
+
+  # def show
+  #   @message = Message.find(params[:id])
+  # end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
   
 end
