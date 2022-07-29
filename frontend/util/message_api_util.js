24355b5f2c7cec448fe5ff95f4bd123c1474bd3d
Michael Chen <michael.chen.1120@gmail.com>
2022-07-29 17:49:39 -0400

    Add create message action

--- frontend/util/message_api_util.js
+++ frontend/util/message_api_util.js
@@ -3,3 +3,5 @@ import axios from 'axios';
 export const fetchMessages = () => axios.get(`/api/messages`);
 export const fetchMessage = (messageId) =>
   axios.get(`/api/messages/${messageId}`);
+export const createMessage = (message) =>
+  axios.post(`/api/messages`, { message });
