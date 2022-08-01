8985b363572c08768c9e95f96cac78e1f0949943
Michael Chen <michael.chen.1120@gmail.com>
2022-08-01 16:44:53 -0400

    Add messages to entities reducer

--- frontend/features/slices/entitiesReducer.js
+++ frontend/features/slices/entitiesReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './userSlice';
 import workspacesReducer from './workspaceSlice';
 import chatsReducer from './chatSlice';
+import messagesReducer from './messageSlice';
 
 const entitiesReducer = combineReducers({
   users: userReducer,
   workspaces: workspacesReducer,
   chats: chatsReducer,
+  messages: messagesReducer,
 });
 
 export default entitiesReducer;
