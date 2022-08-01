faf68462330417959da5b5c719c06780bf10d3ea
Michael Chen <michael.chen.1120@gmail.com>
2022-08-01 16:44:07 -0400

    Add extra reducers for messages

--- frontend/features/slices/messagesSlice.js
+++ frontend/features/slices/messagesSlice.js
@@ -9,4 +9,19 @@ export const messagesSlice = createSlice({
   name: 'messages',
   initialState: {},
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(receiveMessages, (state, action) => {
+        return (state = action.payload);
+      })
+      .addCase(receiveMessage, (state, action) => {
+        return (state = { ...state, [action.payload.id]: action.payload });
+      })
+      .addCase(removeMessage, (state, action) => {
+        delete state[action.payload];
+        return state;
+      });
+  },
 });
+
+export default messagesSlice.reducer;
