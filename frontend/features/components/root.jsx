d1064cd72fb1cd7c460cfc0b47d02752162db364
Michael Chen <michael.chen.1120@gmail.com>
2022-08-02 12:35:47 -0400

    Add action cable prop to root

--- frontend/features/components/root.jsx
+++ frontend/features/components/root.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import App from './app';
 import { Provider } from 'react-redux';
 import { HashRouter, BrowserRouter } from 'react-router-dom';
+import actionCable from 'actioncable';
+
+const actionCable = {};
+
+CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable');
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <App cableApp={CableApp} />
     </HashRouter>
   </Provider>
 );
