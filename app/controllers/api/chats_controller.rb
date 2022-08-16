315227df546ecce07a6aa33af44ac55b705b2bf2
Michael Chen <michael.chen.1120@gmail.com>
2022-08-16 12:48:26 -0400

    Add dm logic to chat controller

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -3,14 +3,27 @@ class Api::ChatsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    #placeholder create method
+    
     @chat = Chat.new(chat_params)
+
+    if @chat.chat_type != channel
+      @chat.name = SecureRandom::urlsafe_base64
+      @chat.description = SecureRandom::urlsafe_base64
+    end
+
     if @chat.save
-      UserChat.create!(chat_id: @chat.id, user_id: current_user.id)
+      if @chat.chat_type == 'channel'
+        UserChat.create!(chat_id: @chat.id, user_id: current_user.id)
+      else
+        params['chat']['users'].each do |user|
+          UserChat.create!(chat_id: @chat.id, user_id: user.id)
+        end
+      end
       render 'api/chats/show'
     else
       render json: @chat.errors.full_messages, status: 422
     end
+
   end
 
   def index
