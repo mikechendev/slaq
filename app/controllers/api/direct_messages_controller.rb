ca16c625a4e855ea12553794fbaebd89988aff76
Michael Chen <michael.chen.1120@gmail.com>
2022-08-22 15:40:05 -0400

    Work on dm controller

--- app/controllers/api/direct_messages_controller.rb
+++ app/controllers/api/direct_messages_controller.rb
@@ -1,2 +1,25 @@
 class Api::DirectMessagesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @dms = DirectMessage.all
+    render :index
+  end
+
+  def show
+    @dm = DirectMessage.find(params[:id])
+    render :show
+  end
+
+  def create
+    @dm = DirectMessage.new(dm_params)
+    @dm.admin_id = current_user.id
+  end
+
+  private
+
+  def dm_params
+    params.require(:direct_message).permit(:body)
+  end
 end
