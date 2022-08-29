1266fac29c66f076735872d50ed8e3b6b6feb784
Michael Chen <michael.chen.1120@gmail.com>
2022-08-29 17:36:49 -0400

    Add edit message component

---
+++ frontend/features/components/messages/edit_message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { updateMessage, removeMessage } from '../../../util/message_api_util';
+
+const EditMessage = (props) => {
+  const dispatch = useDispatch();
+};
+
+export default EditMessage;
