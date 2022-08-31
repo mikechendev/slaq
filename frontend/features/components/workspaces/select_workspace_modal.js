b49370c97d26ba37779cbbff1e14f063c0237538
Michael Chen <michael.chen.1120@gmail.com>
2022-08-31 19:50:21 -0400

    Workspace redirect working

--- frontend/features/components/workspaces/select_workspace_modal.js
+++ frontend/features/components/workspaces/select_workspace_modal.js
@@ -17,20 +17,28 @@ const SelectWorkspaceModal = (props) => {
     (workspace) => workspace.id !== currentWorkspace.id
   );
 
-  const workspacesList = otherWorkspaces.map((workspace) => (
-    <div
-      key={workspace.id}
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        paddingBottom: '2%',
-        fontSize: '18px',
-      }}
-    >
-      <li>{workspace.name}</li>
-      <button style={{ marginRight: '2%' }}>Switch</button>
-    </div>
-  ));
+  const workspacesList = otherWorkspaces.map((workspace) => {
+    let wsChats = Object.values(workspace.chats);
+    let wsLink = () => {
+      return history.go(`/client/${workspace.id}/${wsChats[0].id}`);
+    };
+    return (
+      <div
+        key={workspace.id}
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          paddingBottom: '2%',
+          fontSize: '18px',
+        }}
+      >
+        <li>{workspace.name}</li>
+        <button style={{ marginRight: '2%' }} onClick={wsLink}>
+          Switch
+        </button>
+      </div>
+    );
+  });
 
   return (
     <ReactModal
