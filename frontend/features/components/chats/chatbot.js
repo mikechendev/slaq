c855f209e7cbc8af4e63b2238b3afe9b9ec8bbc0
Michael Chen <michael.chen.1120@gmail.com>
2023-05-16 16:32:18 -0400

    rename chatbot component add react

--- frontend/features/components/chats/chatbot.js
+++ frontend/features/components/chats/chatbot.js
@@ -2,6 +2,7 @@ import { config } from 'dotenv';
 config();
 
 import { Configuration, OpenAIApi } from 'openai';
+import React, { useState } from 'react';
 
 const openai = new OpenAIApi(
   new Configuration({ apiKey: process.env.OPENAI_API_KEY })
@@ -15,3 +16,17 @@ openai
   .then((response) => {
     console.log(response.data.choices[0].message.content);
   });
+
+const ChatBot = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+};
+
+return(
+  <div>
+  </div>
+)
