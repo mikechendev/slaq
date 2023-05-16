6d6b0a79182da164ad8ab3035c9c5819ad263fe9
Michael Chen <michael.chen.1120@gmail.com>
2023-05-16 19:59:44 -0400

    Add send message function

--- frontend/features/components/chats/chatbot.js
+++ frontend/features/components/chats/chatbot.js
@@ -8,14 +8,14 @@ const openai = new OpenAIApi(
   new Configuration({ apiKey: process.env.OPENAI_API_KEY })
 );
 
-openai
-  .createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [{ role: 'user', content: 'Hello, how are you?' }],
-  })
-  .then((response) => {
-    console.log(response.data.choices[0].message.content);
-  });
+// openai
+//   .createChatCompletion({
+//     model: 'gpt-3.5-turbo',
+//     messages: [{ role: 'user', content: 'Hello, how are you?' }],
+//   })
+//   .then((response) => {
+//     console.log(response.data.choices[0].message.content);
+//   });
 
 const ChatBot = () => {
   const [messages, setMessages] = useState([]);
@@ -24,9 +24,14 @@ const ChatBot = () => {
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
+
+  const sendMessage = async () => {
+    if (inputValue.trim() === '') return;
+
+    setMessages([...messages, { text: inputValue, user: true }]);
+    setInputValue('');
+    
+  };
 };
 
-return(
-  <div>
-  </div>
-)
+return <div></div>;
