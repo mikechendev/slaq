3eac348c570b3f67913ea9fe47c86e1322cd25af
Michael Chen <michael.chen.1120@gmail.com>
2023-05-25 21:20:06 -0400

    Work on chatbot component

--- frontend/features/components/chats/chatbot.js
+++ frontend/features/components/chats/chatbot.js
@@ -3,42 +3,42 @@ config();
 
 import { Configuration, OpenAIApi } from 'openai';
 import readline from 'readline';
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 const openai = new OpenAIApi(
   new Configuration({ apiKey: process.env.OPENAI_API_KEY })
 );
 
-const userInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const userInterface = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-userInterface.prompt();
-userInterface.on('line', async (input) => {
-  const res = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [{ role: 'user', content: input }],
-  });
-  console.log(res.data.choices[0].message.content);
-  userInterface.prompt();
-});
+// userInterface.prompt();
+// userInterface.on('line', async (input) => {
+//   const res = await openai.createChatCompletion({
+//     model: 'gpt-3.5-turbo',
+//     messages: [{ role: 'user', content: input }],
+//   });
+//   console.log(res.data.choices[0].message.content);
+//   userInterface.prompt();
+// });
 
-// const ChatBot = () => {
-//   const [messages, setMessages] = useState([]);
-//   const [inputValue, setInputValue] = useState('');
+const ChatBot = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-//   const handleInputChange = (e) => {
-//     setInputValue(e.target.value);
-//   };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-//   const sendMessage = async () => {
-//     if (inputValue.trim() === '') return;
+  const sendMessage = async () => {
+    if (inputValue.trim() === '') return;
 
-//     setMessages([...messages, { text: inputValue, user: true }]);
-//     setInputValue('');
+    setMessages([...messages, { text: inputValue, user: true }]);
+    setInputValue('');
 
-//   };
-// };
+  };
+};
 
-// return <div></div>;
+return <div></div>;
